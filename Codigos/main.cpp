239475af240cbaa77bc69e17432f79415cae48d6
Pizzanami <itzanami.berlanga@gmail.com>
2020-12-13 19:20:55 -0600

    Intro

--- Codigos/main.cpp
+++ Codigos/main.cpp
@@ -5,7 +5,7 @@
 #define DIF 0
 #define TAM_S 100
 #define VAL_I 5
-#define WX 200
+#define WX 220
 #define WY 510
 Window ventana("Totoro Dancing Life", 500, 550);
 
@@ -15,15 +15,18 @@ typedef struct{
 }Image;
 
 Image FramesTotoro[N_F];
+Image FramesIntro[2];
 int secuencia[100];
 
 Image createImage(char *name);
 void freeImage(Image *img);
 void secuenciaSalida();
+int intro();
 void brinco1();
 void brinco2();
 void brinco3();
 void fallaste();
+void cleanLinea(int y);
 void perdiste(int score);
 void repiteSecuencia(int score);
 int revisaSecuencia(int score);
@@ -32,6 +35,8 @@ void iniciaSecuencia();
 int main(){	
 	srand(time(NULL));
 	int i;
+	
+	
 	
 	for(i = 0; i < N_F; i++){
 		char filename[100];
@@ -48,28 +53,36 @@ int main(){
 		LeeBmpColor(FramesTotoro[i].imagen, filename);
 	}
 	
+	for(i = 0; i < 2; i++){
+		char name[100];
+		sprintf(name, "Intro0%d.bmp", i);
+
+		// memoria para la imagen i
+		FramesIntro[i] = createImage(name);
+		
+		// leemos en bmp
+		LeeBmpColor(FramesIntro[i].imagen, name);
+	}
+	
+	ventana.printf(180, WY+20, "PULSA X PARA SALIR");
 	i = 0;
 	unsigned int key = 9;
 	int game = -1, score = 0;
 	iniciaSecuencia();
+	if(!intro())
+		return 0;
 	while(1){
 		// Kbhit() regresa 1 si el usuario presiono una tecla en el teclado, regresa 0 si no presiono nada
 		// guarda la tecla presionada en la variable key
 		int resultado = (int) Kbhit(&key);
 		if (key== 88){
 			secuenciaSalida();
-			for(i = 0; i < N_F; i++)
-				freeImage(&FramesTotoro[i]);
 			return 0;
 		}
 		if (resultado == 1){
 			game =1;
 		}
 		if(game){
-			i = 26;
-			ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
-			ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
-			Sleep(200);
 			repiteSecuencia(score);
 			game = 0;
 		}
@@ -92,7 +105,8 @@ int main(){
 	
 	for(i = 0; i < N_F; i++)
 		freeImage(&FramesTotoro[i]);
-	
+	for(i = 0; i < 0; i++)
+		freeImage(&FramesIntro[i]);
 	return MainLoop();
 }
 
@@ -105,8 +119,6 @@ int revisaSecuencia(int score){
 		if(resultado){
 		if (key== 88){
 			secuenciaSalida();
-			for(i = 0; i < N_F; i++)
-				freeImage(&FramesTotoro[i]);
 			return -1;
 		}
 		if(key == 49 || key==70){
@@ -147,8 +159,14 @@ int revisaSecuencia(int score){
 }
 
 void repiteSecuencia(int score){
-	ventana.printf(15, 200, "Hola mundo");
 	int i;
+	cleanLinea(WY);
+	i = 26;
+	ventana.printf(150, WY, "Atencion! Memoriza la secuencia");
+	ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
+	ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
+	Sleep(1000);
+	
 	for(i = 0; i < score+VAL_I; i++){
 		switch(secuencia[i]){
 			case 0:
@@ -180,6 +198,7 @@ void brinco1(){
 	for(i = 4; i < 18; i++){
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
+		cleanLinea(WY);
 		ventana.printf(WX, WY, "Paso 1");
 		Sleep(VEL - DIF);
 	}
@@ -190,6 +209,7 @@ void brinco2(){
 	for(i = 13; i < 18; i++){
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
+		cleanLinea(WY);
 		ventana.printf(WX, WY, "Paso 2");
 		Sleep(VEL - DIF);
 	}
@@ -200,6 +220,7 @@ void brinco3(){
 	for(i = 14; i < 18; i++){
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
+		cleanLinea(WY);
 		ventana.printf(WX, WY, "Paso 3");
 		Sleep(VEL - DIF);
 	}
@@ -210,12 +231,18 @@ void fallaste(){
 	for(i = 18; i < 27; i++){
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
+		cleanLinea(WY);
 		ventana.printf(WX, WY, "Fallastee!");
 		Sleep(VEL - DIF);
 	}
 }
 
 void secuenciaSalida(){
+	int i;
+	for(i = 0; i < N_F; i++)
+		freeImage(&FramesTotoro[i]);
+	for(i = 0; i < 0; i++)
+		freeImage(&FramesIntro[i]);
 	return;
 }
 
@@ -251,7 +278,32 @@ void perdiste(int score){
 	for(i = 18; i < 27; i++){
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, V_REC, 0, 1.0);
 		ventana.PlotCBitmap(FramesTotoro[i].imagen, FramesTotoro[i].nr, FramesTotoro[i].nc, 500+V_REC, 0, 1.0);
-		ventana.printf(WX, WY, "Perdiste! Tu puntuacion final es %d", score);
+		cleanLinea(WY);
+		ventana.printf(150, WY, "Perdiste! Tu puntuacion final es %d", score);
 		Sleep(VEL - DIF);
 	}
+}
+
+int intro(){
+	int resultado = 0;
+	int i = 0;
+	unsigned int key;
+	while(!resultado){
+		resultado = (int) Kbhit(&key);
+		ventana.PlotCBitmap(FramesIntro[i].imagen, FramesIntro[i].nr, FramesIntro[i].nc, V_REC, 0, 1.0);
+		ventana.PlotCBitmap(FramesIntro[i].imagen, FramesIntro[i].nr, FramesIntro[i].nc, 500+V_REC, 0, 1.0);
+		i = (i+1)%2;
+		if(key==88){
+			secuenciaSalida();
+			return 0;
+		}
+		ventana.printf(100, WY, "PULSA CUALQUIER TECLA PARA CONTINUAR");
+		Sleep(300);
+	}
+	cleanLinea(WY);
+	return 1;
+}
+
+void cleanLinea(int y){
+	ventana.printf(0, y, "                                                                                                                                                       ");
 }
